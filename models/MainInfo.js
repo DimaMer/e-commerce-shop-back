d5ba98d4e289aab0a29886f83ea373205da28e9a
Dima <dimm1simm1@gmail.com>
2020-01-23 16:10:17 +0200

    phones mainInfo

--- models/MainInfo.js
+++ models/MainInfo.js
@@ -5,7 +5,9 @@ const MainInfoSchema = new Schema({
   title: { type: String, required: true },
   titleLong: { type: String, required: true },
   address: {type: String, },
-  phones: [ {type: String, min: 6, } ],
+  phones1: {type: String, min: 6, },
+  phones2: {type: String, min: 6, },
+  phones3: {type: String, min: 6, },
   email: { type: String,  },
   photo: { type: String, }
 });
