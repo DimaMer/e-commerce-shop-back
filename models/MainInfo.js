c76e818ad3a2aff4fe1ac2d5113210a88d38da39
Dima <dimm1simm1@gmail.com>
2020-02-03 12:53:19 +0200

    localization

--- models/MainInfo.js
+++ models/MainInfo.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const MainInfoSchema = new Schema({
-  title: { type:{ ua: String, ru: String, en: String }, required: true },
-  titleLong: { type:{ ua: String, ru: String, en: String }, required: true },
-  address: { type:{ ua: String, ru: String, en: String }, required: true },
+  title: { type:{ ua: String, ru: String, en: String } },
+  titleLong: { type:{ ua: String, ru: String, en: String } },
+  address: { type:{ ua: String, ru: String, en: String } },
   phones1: {type: String, min: 6, },
   phones2: {type: String, min: 6, },
   phones3: {type: String, min: 6, },
