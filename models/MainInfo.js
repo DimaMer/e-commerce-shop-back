9d94bc176e1a68e6d8efb84fbf5cc28a694dac42
Dima <dimm1simm1@gmail.com>
2020-02-03 14:47:12 +0200

    mainInfo default

--- models/MainInfo.js
+++ models/MainInfo.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const MainInfoSchema = new Schema({
-  title: { type:{ ua: String, ru: String, en: String } },
-  titleLong: { type:{ ua: String, ru: String, en: String } },
-  address: { type:{ ua: String, ru: String, en: String } },
-  phones1: {type: String, min: 6, },
-  phones2: {type: String, min: 6, },
-  phones3: {type: String, min: 6, },
-  email: { type: String,  },
-  photo: { type: String, }
+  title: { type:{ ua: String, ru: String, en: String }, default: { ua: "", ru: "", en: "" } },
+  titleLong: { type:{ ua: String, ru: String, en: String }, default: { ua: "", ru: "", en: "" } },
+  address: { type:{ ua: String, ru: String, en: String }, default: { ua: "", ru: "", en: "" } },
+  phones1: {type: String, min: 6, default: "123456" },
+  phones2: {type: String, min: 6, default: "123456" },
+  phones3: {type: String, min: 6, default: "123456" },
+  email: { type: String,  default: "email" },
+  photo: { type: String, default: "" }
 });
 
 const MainInfo = mongoose.model('MainInfo', MainInfoSchema);
