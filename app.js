9de1dcf6597d1b2b6454ef33fdffa9962a38639a
Dima <dimm1simm1@gmail.com>
2020-02-07 10:36:18 +0200

    sesion

--- app.js
+++ app.js
@@ -55,9 +55,7 @@ app.use(session({
   saveUninitialized: true,
     sameSite: 'strict',
     secure: true,
-  cookie: {maxAge: 24 *60 *60 * 1000,
-      sameSite: 'strict',
-      secure: true},
+  cookie: {maxAge: 24 *60 *60 * 1000},
   httpOnly: true
 }));
 
