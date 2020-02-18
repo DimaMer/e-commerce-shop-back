81e8444159cf3097d7b68202a2df4c5dcc58ffcc
Dima <dimm1simm1@gmail.com>
2020-02-18 14:46:59 +0200

    file.originalname.replace

--- helpers/multer.js
+++ helpers/multer.js
@@ -52,7 +52,7 @@ const storage = multer.diskStorage({
     return cb(null, `public/${file.fieldname}`);
   },
   filename: (req, file, cb) => {
-      return cb(null, `${file.fieldname}-${Date.now()}-${file.originalname.replace("_", " ")}`);
+      return cb(null, `${file.fieldname}-${Date.now()}-${file.originalname.replace(" ", "_")}`);
   },
 });
 
